bbb5b72850e093fb11268309b3bd1792afab69aa
supasai(boy) <boyskypart@gmail.com>
2025-01-19 02:07:16 +0700

    Initial Commit

---
+++ PhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoApp
+//
+//  Created by supasai haplakhon on 19/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PhotoAppTests/PhotoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAppTests.swift
+//  PhotoAppTests
+//
+//  Created by supasai haplakhon on 19/1/25.
+//
+
+import Testing
+@testable import PhotoApp
+
+struct PhotoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
